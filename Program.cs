6fd8c15101051f1fbfcf9a0a766b08f71dbe2ae3
rezadhied <rezadhied@student.telkomuniversity.ac.id>
2023-03-12 19:58:40 +0700

    menambahkan implementasi dengan state-based construction

--- Program.cs
+++ Program.cs
@@ -2,6 +2,29 @@
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 
+class main
+{
+    static void Main(string[] args)
+    {
+        KodePos.Kelurahan k = KodePos.Kelurahan.Batununggal;
+        Console.WriteLine("Kode pos kelurahan " + k + " adalah " + KodePos.getKodePos(k));
+        KodePos.Kelurahan k2 = KodePos.Kelurahan.Mengger;
+        Console.WriteLine("Kode pos kelurahan " + k2 + " adalah " + KodePos.getKodePos(k2));
+        KodePos.Kelurahan k3 = KodePos.Kelurahan.Samoja;
+        Console.WriteLine("Kode pos kelurahan " + k3 + " adalah " + KodePos.getKodePos(k3));
+
+        Console.WriteLine("=======Door Machine=======");
+
+        DoorMachine pintu = new DoorMachine();
+
+        pintu.KunciPintu();
+        pintu.BukaPintu();
+        pintu.BukaPintu(); 
+        pintu.KunciPintu();
+        pintu.BukaPintu(); 
+    }
+}
+
 public class KodePos
 {
     public enum Kelurahan {Batununggal, Kujangsari, Mengger, Wates, 
@@ -15,17 +38,29 @@ public class KodePos
     }
 }
 
-class main
+public class DoorMachine
 {
-    static void Main(string[] args)
+    public enum State {Terkunci, Terbuka };
+
+    private State state = State.Terkunci;
+
+    public void KunciPintu()
     {
-        KodePos.Kelurahan k = KodePos.Kelurahan.Batununggal;
-        Console.WriteLine("Kode pos kelurahan "+k+" adalah "+KodePos.getKodePos(k));
-        KodePos.Kelurahan k2 = KodePos.Kelurahan.Mengger;
-        Console.WriteLine("Kode pos kelurahan " + k2 + " adalah " + KodePos.getKodePos(k2));
-        KodePos.Kelurahan k3 = KodePos.Kelurahan.Samoja;
-        Console.WriteLine("Kode pos kelurahan " + k3 + " adalah " + KodePos.getKodePos(k3));
+        state = State.Terkunci;
+        Console.WriteLine("Pintu Terkunci");
+    }
+
+    public void BukaPintu()
+    {
+        switch (state)
+        {
+            case State.Terkunci:
+                state = State.Terbuka;
+                Console.WriteLine("Pintu tidak terkunci");
+                break; 
+             case State.Terbuka:
+                Console.WriteLine("Pintu tidak terkunci");
+                break;
+        }
     }
 }
-
-
